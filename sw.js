da9bc4c0305896d8e8a285b6ca027c6ec4bac51b
raadalmosa2000 <raadalmosa1@gmail.com>
2026-08-12 06:51:21 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,84 @@
-const CACHE='alburj-v2';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./style.css','./manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE_NAME = "alburj-final-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json"
+];
+
+
+self.addEventListener(
+  "install",
+  function (event) {
+
+    event.waitUntil(
+
+      caches
+        .open(CACHE_NAME)
+        .then(function (cache) {
+
+          return cache.addAll(FILES);
+
+        })
+
+    );
+
+    self.skipWaiting();
+
+  }
+);
+
+
+self.addEventListener(
+  "activate",
+  function (event) {
+
+    event.waitUntil(
+
+      caches.keys().then(function (keys) {
+
+        return Promise.all(
+
+          keys
+            .filter(function (key) {
+
+              return key !== CACHE_NAME;
+
+            })
+            .map(function (key) {
+
+              return caches.delete(key);
+
+            })
+
+        );
+
+      })
+
+    );
+
+    self.clients.claim();
+
+  }
+);
+
+
+self.addEventListener(
+  "fetch",
+  function (event) {
+
+    event.respondWith(
+
+      caches.match(event.request)
+        .then(function (cached) {
+
+          return cached || fetch(event.request);
+
+        })
+
+    );
+
+  }
+);
